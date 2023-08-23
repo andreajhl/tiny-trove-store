ad8fa008c38409eee40561222d1cab9b69075756
Andrea Hernandez <andreajhl29@gmail.com>
2023-08-23 02:43:31 -0300

    feat: :wrench: add file config

---
+++ jest.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  collectCoverage: true,
+  coverageProvider: 'v8',
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!<rootDir>/out/**',
+    '!<rootDir>/.next/**',
+    '!<rootDir>/*.config.js',
+    '!<rootDir>/coverage/**',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/.next/',
+    '<rootDir>/wordings/',
+    '<rootDir>/mocks/',
+    '<rootDir>/interfaces/',
+    '<rootDir>/types/',
+    '<rootDir>/hooks/',
+  ],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/mocks/',
+    '<rootDir>/wordings/',
+    '<rootDir>/interfaces/',
+    '<rootDir>/types/',
+    '<rootDir>/hooks/',
+  ],
+  collectCoverage: true,
+  coverageDirectory: './coverage/',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+}
