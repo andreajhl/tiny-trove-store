5218b153392b42cff05e15b9d2f912bc043ccc8d
Andrea Hernandez <andreajhl29@gmail.com>
2023-08-23 03:29:42 -0300

    add bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import { CartProvider } from "@/context/cart";
 import { AppProps } from "next/app";
+import Script from "next/script";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <CartProvider>
-    <Component { ...pageProps } />
-  </CartProvider>
+  <> 
+    <Script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+      crossOrigin="anonymous"
+    />
+    <CartProvider>
+      <Component { ...pageProps } />
+    </CartProvider>
+  </>
 )
 
 export default MyApp;
