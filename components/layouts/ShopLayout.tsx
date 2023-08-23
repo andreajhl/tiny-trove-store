72958168859fdf40fe042fc7da907f3fa6a56145
Andrea Hernandez <andreajhl29@gmail.com>
2023-08-23 02:52:02 -0300

    feat: :sparkles: add layout

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import { ShopLayoutProps } from "@/interfaces/components/layouts";
+import NavBar from "../ui/NavBar";
+
+const ShopLayout = ({ children, title, pageDescription, imageFullUrl }: ShopLayoutProps) => (
+  <>
+    <Head>
+      <title>{title}</title>
+      <meta name="author" content="Andrea Hernandez" />
+      <meta name="description" content={pageDescription} />
+      <meta name="keywords" content="baby, diapers, cribs, strollers, toys, baby clothes, baby products,items for newborns, baby shopping, bottles, educational toys" />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={pageDescription} />
+      { imageFullUrl && <meta name="og:image" content={imageFullUrl} /> }
+    </Head>
+    <nav>
+      <NavBar />
+    </nav>
+    <main>
+      {children}
+    </main>
+    <footer>
+
+    </footer>
+  </>
+);
+
+export default ShopLayout;
