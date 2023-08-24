fe4bacbc774426f5c6d76a4ee58d7864daaabd29
Andrea Hernandez <andreajhl29@gmail.com>
2023-08-23 22:26:37 -0300

    feat: :lipstick: fix responsive cards

--- components/pageMain/index.tsx
+++ components/pageMain/index.tsx
@@ -5,28 +5,31 @@ import BannerCategories from "../categories/bannerCategories";
 import Carousel from "../ui/Carousel";
 import { carouselImages } from "@/constants/carousel";
 import wordings from "@/wordings";
+import './styles.scss';
 
 const PageMain = () => {
   const { home: { banner: { creditCard, warranty, store } } } = wordings;
 
   return (
-    <main>
-      <Carousel pictures={carouselImages} />
-      <div className="d-flex justify-content-center align-items-center pt-2 w-100">
-        <div className="text-center border-end border-light-subtle w-25" data-testid="card-info">
+    <main className="page-main">
+      <div className="page-main-carousel">
+        <Carousel pictures={carouselImages} />
+      </div>
+      <div className="page-main-content">
+        <div className="page-main-content__card border-end border-light-subtle" data-testid="card-info">
           <h3><BsCreditCard2Back /></h3>
-          <h3 className="fw-bold text-info">{creditCard.title}</h3>
-          <h4 className="fw-lighter text-secondary-emphasis">{creditCard.subtitle}</h4>
+          <h3 className="page-main-content__card-title">{creditCard.title}</h3>
+          <h4 className="page-main-content__card-subtitle">{creditCard.subtitle}</h4>
         </div>
-        <div className="text-center border-end border-light-subtle w-25" data-testid="card-info">
+        <div className="page-main-content__card border-end border-light-subtle" data-testid="card-info">
           <h3><GoShieldCheck /></h3>
-          <h3 className="fw-bold text-info">{warranty.title}</h3>
-          <h4 className="fw-lighter text-secondary-emphasis">{warranty.subtitle}</h4>
+          <h3 className="page-main-content__card-title">{warranty.title}</h3>
+          <h4 className="page-main-content__card-subtitle">{warranty.subtitle}</h4>
         </div>
-        <div className="text-center w-25" data-testid="card-info">
+        <div className="page-main-content__card" data-testid="card-info">
           <h3><PiMapPinLineLight /></h3>
-          <h3 className="fw-bold text-info">{store.title}</h3>
-          <h4 className="fw-lighter text-secondary-emphasis">{store.subtitle}</h4>
+          <h3 className="page-main-content__card-title">{store.title}</h3>
+          <h4 className="page-main-content__card-subtitle">{store.subtitle}</h4>
         </div>
       </div>
       <BannerCategories />
